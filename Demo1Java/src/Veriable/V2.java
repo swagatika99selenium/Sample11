43d47e7d51a6225565fc284db14cdd59e586b0ff
swagatika99selenium <typetoswagatika@gmail.com>
2025-04-11 12:57:15 -0400

    Create V2.java
    Sum of 2.

---
+++ Demo1Java/src/Veriable/V2.java
@@ -0,0 +1,13 @@
+package Veriable;
+
+public class V2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=2;
+int b=4;
+int c=a+b;
+System.out.println(c);
+	}
+
+}
